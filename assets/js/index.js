ea23d5806bd403bd5314fbebdd8b2a48108d7b2e
vurah <vurahpersonal@gmail.com>
2025-08-23 11:59:59 +0200

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+
+let titles = ['consist.lol','private','biolink']
+let index = 0
+let interval = setInterval(() => {
+    document.title = titles[index]
+    index += 1
+    if (index === titles.length) index = 0
+}, 500)
