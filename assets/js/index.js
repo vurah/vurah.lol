770ce785ffda3b9643f2889601a7c8ddd99b8fae
vurah <vurahpersonal@gmail.com>
2025-08-23 12:00:26 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
 
-let titles = ['consist.lol','private','biolink']
+let titles = ['vurah.lol','private','biolink']
 let index = 0
 let interval = setInterval(() => {
     document.title = titles[index]
     index += 1
     if (index === titles.length) index = 0
+
 }, 500)
